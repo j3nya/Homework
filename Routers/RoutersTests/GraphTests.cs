57d4f704edf0dc4dd6d0e25fb3dac9b717c58669
j3nya <cvbn0xxx@gmail.com>
2025-03-28 21:52:38 +0300

    Add tests
    Signed-off-by: j3nya <cvbn0xxx@gmail.com>

---
+++ Routers/RoutersTests/GraphTests.cs
@@ -0,0 +1,55 @@
+﻿namespace RoutersTests;
+public class Tests
+{
+    private Graph.Graph graph;
+    [SetUp]
+    public void Setup()
+    {
+        graph = new Graph.Graph(5);
+        graph.AddEdge(3, 1, 1);
+    }
+    [Test]
+    public void CheckIfAddingEdgeAddsEdgeAndAddingVertexAddsVertex()
+    {
+        graph.AddVertex();
+        graph.AddEdge(6, 2, 1);
+        Assert.Multiple (() =>
+        {
+            Assert.That(graph.VertexCount, Is.EqualTo(6));
+            Assert.That(graph.AdjacencyList[5][0], Is.EqualTo((2, 1)));
+        });
+    }
+    public void CheckIfCheckForConnectivityReturnsTrueIfVerticesAreConnected()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(graph.CheckIfConnected(3, 1));
+            Assert.That(graph.CheckIfConnected(1, 3));
+            Assert.That(graph.CheckIfConnected(6, 2));
+        });
+    }
+    public void CheckIfCheckForConnectivityReturnsFalseIfVerticesAreNotConnected()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(!graph.CheckIfConnected(1, 2));
+            Assert.That(!graph.CheckIfConnected(3, 6));
+        });
+    }
+
+    public void CheckIfCheckForConnectivityReturnsTrueIfGraphIsConnected()
+    {
+        graph.AddEdge(1, 2, 1);
+        graph.AddEdge(2, 4, 1);
+        graph.AddEdge(4, 5, 1);
+        Assert.That(graph.CheckIfConnected());
+    }
+    public void CheckIfCheckForConnectivityReturnsFalseIfGraphIsNotConnected()
+    {
+        Assert.That(!graph.CheckIfConnected());
+    }
+    public void CheckIfEdgeIsNotAddedWhenVertex1AndVertex2AreTheSame()
+    {
+        Assert.Throws<Exception>(() => graph.AddEdge(1, 1, 1));
+    }
+}
