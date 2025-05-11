544ba519fc66078245bbdf23679435a5b188559f
j3nya <cvbn0xxx@gmail.com>
2025-05-11 13:03:47 +0300

    update to stylecop 1.2-beta, remove warns

--- Map-Filter-Fold/Map-Filter-Fold/Map-Filter-Fold.cs
+++ Map-Filter-Fold/Map-Filter-Fold/Map-Filter-Fold.cs
@@ -1,5 +1,6 @@
 ﻿// <copyright file="Map-Filter-Fold.cs" company="Chernoshchokaya Evgenia">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+// Copyright (c) Chernoshchokaya Evgenia. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace Map_Filter_Fold;
