43dba2a6b4a56f7d3185e4e636501e904a82cbed
j3nya <cvbn0xxx@gmail.com>
2025-05-30 03:28:59 +0300

    remove files

--- Program.cs
+++
@@ -1,3 +0,0 @@
-﻿string? input = Console.ReadLine();
-(string transformedString, int end) = Burrows_Wheeler_Transform.BWTSolver.BurrowsWheelerTransform(input);
-Burrows_Wheeler_Transform.BWTSolver.ReverseBurrowsWheelerTransform(transformedString, end);
