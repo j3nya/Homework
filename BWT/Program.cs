95b185b2403a86bacd866e3547f172dc29c30688
j3nya <cvbn0xxx@gmail.com>
2025-05-30 03:22:16 +0300

    move files to directory, comments corrected, add file headers

---
+++ BWT/Program.cs
@@ -0,0 +1,10 @@
+﻿string? input = Console.ReadLine();
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("EMPTYSTRING");
+}
+
+(string transformedString, int end) = burrowsWheelerTransform.BWTSolver.BurrowsWheelerTransform(input!);
+Console.WriteLine(transformedString + $" {end}");
+string restoredString = burrowsWheelerTransform.BWTSolver.ReverseBurrowsWheelerTransform(transformedString, end);
+Console.WriteLine(restoredString);
